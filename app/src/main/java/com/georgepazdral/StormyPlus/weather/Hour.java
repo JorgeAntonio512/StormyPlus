eef037199b4045b2e04b180cf1a2c11ee192795b
JorgeAntonio512 <jorge.antonio.de.segundo@gmail.com>
2015-07-31 21:04:26 -0500

    Changed hourly forecast temp from double to int. Also, fixed 7 day forecast listview small icon issue

--- app/src/main/java/com/georgepazdral/StormyPlus/weather/Hour.java
+++ app/src/main/java/com/georgepazdral/StormyPlus/weather/Hour.java
@@ -34,7 +34,7 @@ public class Hour implements Parcelable {
         mSummary = summary;
     }
 
-    public double getTemperature() {
+    public int getTemperature() {
         return (int) Math.round(mTemperature);
     }
 
